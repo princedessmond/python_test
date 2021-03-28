963525fba87179e9e8fbd9e60e04e7dd8ebbbb6b
Prince Mwita <psdesmond@tutanota.com>
2021-03-28 17:56:05 +0300

    deerper systems test

---
+++ solution.py
@@ -0,0 +1,24 @@
+import json
+
+
+with open('source_file_2.json', 'r') as f:
+    data = json.load(f)
+    sorted_data = sorted(data, key=lambda project: project.get('priority'))
+
+    watchers = managers = {}
+    for project in sorted_data:
+        project_name = project.get('name')
+        for manager in project.get('managers'):
+            if not managers.get(manager):
+                managers[manager] = []
+            managers[manager].append(project_name)
+        for watcher in project.get('watchers'):
+            if not watchers.get(watcher):
+                watchers[watcher] = []
+            watchers[watcher].append(project_name)
+
+with open("managers.json", "w") as managers_file:
+    json.dump(managers, managers_file)
+
+with open("watchers.json", "w") as watchers_file:
+    json.dump(watchers, watchers_file)
